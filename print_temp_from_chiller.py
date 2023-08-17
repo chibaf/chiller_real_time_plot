0bcc358d79dc3eaa616cffc2c76ac591014ec19d
chibaf <chibaf@mac.com>
2023-08-17 19:18:37 +0900

    Add files via upload

--- print_temp_from_chiller.py
+++ print_temp_from_chiller.py
@@ -3,6 +3,7 @@
 import time
 #import matplotlib.pyplot as plt
 import serial
+import sys
 
 ser = serial.Serial(
     port='/dev/ttyUSB0',  # ポート名は環境に合わせて変更
@@ -10,6 +11,8 @@ ser = serial.Serial(
     timeout=1  # タイムアウトの設定
 )
 
+if len(sys.argv) ==2: # open an output file as name=sys.argv[1]
+  f = open(sys.argv[1], "w")
 temps=[0]*10
 x=range(0, 10, 1)
 request_00M1 = b'\x04\x30\x30\x4D\x31\x05'
@@ -24,6 +27,8 @@ while True:
     temp=temp[:-2]   # remove the last two character
     temps.pop(-1)    # remove the last element
     temps.insert(0,float(temp)) # add element to the first position
+    if len(sys.argv) ==2: # write data to file
+      f.write(str(temps[0])+","+str(temps[1])+","+str(temps[2])+","+str(temps[3])+","+str(temps[4])+","+str(temps[5])+","+str(temps[6])+","+str(temps[7])+","+str(temps[8])+","+str(temps[9])+"\n")
     print(temps)      
 #      plt.clf()
 #      plt.plot(x,rez[0])
@@ -31,5 +36,7 @@ while True:
   except KeyboardInterrupt:
     print ('exiting')
     ser.close()
+    if len(sys.argv) ==2: 
+      f.close()
     break
 exit()
